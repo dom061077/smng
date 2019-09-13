17326907b7d862e5f26a2ce90c6b55bac6ff9185
dom061077 <dom061077@gmail.com>
2019-09-13 20:08:58 -0300

    security

--- src/app/security/change.password.component.ts
+++ src/app/security/change.password.component.ts
@@ -11,21 +11,41 @@ import {MessageService} from 'primeng/api';
 
 export class ChangePasswordComponent  implements OnInit{
     changePassForm: FormGroup;
+    submitted: boolean;
     constructor(private fb: FormBuilder, private messageService: MessageService) {
         console.log("Constructor changepassword");
     }
     ngOnInit() {
-        this.changePassForm = this.fb.group({
-            'password': new FormControl('', Validators.required)//,
+        /*this.changePassForm = this.fb.group({
+            'passwordAnterior': new FormControl('', Validators.required),
+            'password': new FormControl('',Validators.required),
+            'confirmPassword': new FormControl('',Validators.required)
             //'lastname': new FormControl('', Validators.required),
             //'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
             //'description': new FormControl(''),
             //'gender': new FormControl('', Validators.required)
-        });
+        });*/
+        this.changePassForm = this.fb.group({
+            passwordAnterior:[],
+            password:[],
+            confirmPassword:[]
+        },{});
 
        /* this.genders = [];
         this.genders.push({label:'Select Gender', value:''});
         this.genders.push({label:'Male', value:'Male'});
         this.genders.push({label:'Female', value:'Female'});*/
     }
+    onSubmit(value: string) {
+        this.submitted = true;
+        this.messageService.add({severity:'info', summary:'Success', detail:'Form Submitted'});
+    }    
+
+    validarNewPassword(){
+
+    }
+
+
+
+
 }
