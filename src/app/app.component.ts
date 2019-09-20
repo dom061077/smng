f61ee273b975d41c03c8e21fd3ae4ad0a1c5073c
dom061077 <dom061077@gmail.com>
2019-09-20 20:18:35 -0300

    security

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RestUserDataSource } from './model/rest.user.datasource';
 import {MessageService} from 'primeng/api';
+import { AuthenticationService } from './security/authentication.service';
 
 
 
@@ -13,12 +14,21 @@ import {MessageService} from 'primeng/api';
 export class AppComponent {
   title = 'smng';
   
-  constructor(private dsuser:RestUserDataSource,private messageService:MessageService){
+  
+  constructor(private dsuser:RestUserDataSource,private messageService:MessageService
+      ,private authService:AuthenticationService){
       console.log("constructor AppComponent XXXXXX");
+      authService.currentRestSubError.subscribe(data=>{
+          if(data!=null){
+            this.messageService.clear;
+            this.messageService.add({severity:'error',summary:data.msgobj.title,detail:data.msgobj.msg});
+          }
+
+      });
   }
 
   public login() {
     this.messageService.add({severity:'error', summary:'Mensaje', detail:'prueba de toast'});
-}  
+  }  
 
 }
--- src/app/security/jwt.interceptor.ts
+++ src/app/security/jwt.interceptor.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 import { AuthenticationService } from './authentication.service';
 import { catchError } from 'rxjs/operators';
-import { throw } from 'rxjs/observable/throw';
+import {MessageService} from 'primeng/api';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) { }
+    constructor(private authenticationService: AuthenticationService 
+        ,private msgService:MessageService,private router:Router) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       // add authorization header with jwt token if available
       let currentUser = this.authenticationService.currentUserValue;
@@ -25,7 +27,38 @@ export class JwtInterceptor implements HttpInterceptor {
       return next.handle(request)
         .pipe(
             catchError(error =>{
-                return _throw (error);
+                if (error.status == 401){
+                    // msg = 'Usuario o Contraseña incorrectos';
+                    // title = 'Error';
+                    // if(currentUser && currentUser.access_token){
+                        console.log('Se envió un mensaje de ERROR!!!');
+                        this.router.navigateByUrl("/login");
+                        //error=null;
+                    // }else{                    
+                         error.msgobj = {title:'Error',msg:'Usuario o contraseñ incorrectos'}
+                    // }
+                    
+                }           
+                if(error.status == 400){
+
+                        error.msgobj = {title:'Error',msg:'Ingrese usuario y contraseña'}
+                    
+                }     
+                if(error.status == 422){
+                    // title = 'Error en ingreso de datos';
+                    // if(error.error.total>1){
+                    //     msg='<ul>'
+                    //     error.error._embedded.errors.forEach(element=>{
+                    //         msg=msg+'<li>'+element.message+'</li>';
+                    //     });     
+                    //     msg=msg+'</ul>';                                       
+                    // }else{
+                    //     msg = msg+ '<ul><li>'+error.error.message+'</li></ul>';
+                    // }
+                }
+                this.authenticationService.currentRestSubError.next(error);
+
+                return throwError (error);
             })
             
         );
