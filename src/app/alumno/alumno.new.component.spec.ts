632605117c7a4bab42f97280667f7188b3b31318
dom061077 <dom061077@gmail.com>
2020-04-04 13:08:29 -0300

    test

--- src/app/alumno/alumno.new.component.spec.ts
+++ src/app/alumno/alumno.new.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject,ComponentFixture,async} from '@angular/core/testing';
-import {RouterTestingModule} from "@angular/router/testing";
+
 import { NO_ERRORS_SCHEMA  } from '@angular/core';
 import { FormsModule,ReactiveFormsModule  } from "@angular/forms";
 import { AlumnoNew } from './alumno.new.component';
@@ -12,9 +12,21 @@ import {AutoCompleteModule} from 'primeng/autocomplete';
 import {CheckboxModule} from 'primeng/checkbox';
 import {CalendarModule} from 'primeng/calendar';
 import {KeyFilterModule} from 'primeng/keyfilter';
-import {Subject} from 'rxjs';
+import {Subject,of} from 'rxjs';
 import {Router,ActivatedRoute} from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { RestDataSource,REST_URL } from "../services/rest.datasource"; 
+import {MessageService} from 'primeng/api';
+import {Validators,ValidationErrors,ValidatorFn,AbstractControl,FormControl,FormGroup,FormBuilder} from '@angular/forms';
+
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import {RouterTestingModule} from "@angular/router/testing"; 
+
+class MockAuthService extends AlumnoService {
+  isAuthenticated() {
+    return 'Mocked';
+  }
+}
 
 describe('AlamnoNew, Create and Edit Alumno',()=>{
     let mockRoute: any = { snapshot: {}};
@@ -27,6 +39,8 @@ describe('AlamnoNew, Create and Edit Alumno',()=>{
     let component: AlumnoNew;
     let element : HTMLElement;
     let fixture : ComponentFixture<AlumnoNew>;
+    let alumnoService : AlumnoService;
+
     /*beforeEach(()=>{
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule
@@ -42,23 +56,40 @@ describe('AlamnoNew, Create and Edit Alumno',()=>{
     });*/
 
     beforeEach(() => {    
+        TestBed.initTestEnvironment(BrowserDynamicTestingModule
+            , platformBrowserDynamicTesting());
+
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule
-                ,ReactiveFormsModule,FormsModule,AutoCompleteModule
+                ,FormsModule,AutoCompleteModule
                 ,RouterTestingModule
                 ,BrowserAnimationsModule
                 ,KeyFilterModule,CheckboxModule,CommonModule
+                
                 ],
             declarations:[AlumnoNew],    
-            providers: [AlumnoService//,CrudCodes,DatePipe
+            providers: [ DatePipe,MessageService
+                    ,FormBuilder
+                    ,{
+                        provide: ActivatedRoute,
+                        useValue: {
+                            params: of({id: 123})
+                        }                        
+                    }
+                    ,{provide:AlumnoService
+                        ,useValue:new AlumnoService(null,null)}
                     //,{provide:ActivatedRoute,useValue:mockRoute}
+                    ,{ provide: REST_URL, useValue: `http://localhost:8080/smr/api` }
                     ],
             schemas: [NO_ERRORS_SCHEMA]
-        });
+        }).compileComponents()
+            .then(()=>{
+                fixture = TestBed.createComponent(AlumnoNew);
+                //component = fixture.componentInstance; // The component instantiation 
+                //element = fixture.nativeElement; // The HTML reference
+            });
 
-        fixture = TestBed.createComponent(AlumnoNew);
-        component = fixture.componentInstance; // The component instantiation 
-        element = fixture.nativeElement; // The HTML reference
+
     });     
 
     it('should create with Mode Alta',()=>{
