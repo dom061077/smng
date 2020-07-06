d9b522c6eaee11dd2097f4d6863a0020c6449081
dom061077 <dom061077@gmail.com>
2020-07-05 23:45:20 -0300

    carga examen

--- src/app/academico/carga.examen.model.ts
+++ src/app/academico/carga.examen.model.ts
@@ -2,7 +2,10 @@ import {Object} from '../model/object.model';
 
 export class CargaExamen extends Object{
     constructor (public id:number , public apellido:string
-        ,nombre:string,cursoDivision:string){
+        ,nombre:string,curso:string,division:string
+        ,periodoEvaluacion:string,promedio:number
+        ,complementario:number
+        ){
             super();
     }
 }
