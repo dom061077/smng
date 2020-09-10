d130872ad4bbc4d2b9948b18c47b75901f350c89
dom061077 <dom061077@gmail.com>
2020-09-09 23:52:38 -0300

    dom061077@gmail.com

--- src/app/academico/carga.examen.promedio.component.ts
+++ src/app/academico/carga.examen.promedio.component.ts
@@ -3,7 +3,7 @@ import {Validators,ValidationErrors,ValidatorFn,AbstractControl,FormControl,Form
 import {MessageService} from 'primeng/api';
 import {AcademicoService} from './academico.service';
 import {Router,ActivatedRoute} from "@angular/router";
-import {Observable } from 'rxjs';
+import {Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {CustomValidators} from '../util/custom-validators';
 
@@ -18,15 +18,16 @@ export class CargaExamenPromedio implements OnInit{
     examenesForm:FormGroup;
     Object = Object;
     //examenes:any;//Observable <Array<any>>;
-    periodosEval:any={
-        periodos:[],
-        keyFormControls:[],
-        labelArray:[]
-    };
-    alumno:any;
-    asignatura:any={};
-    labelArray=[];
-    examKeys=[];
+    periodosEval:BehaviorSubject<any> = new BehaviorSubject({
+           periodos:[] ,
+           keyFormControls:[],
+           labelArray:[],
+    });
+    alumno:BehaviorSubject<any> = new BehaviorSubject({});
+    asignatura:BehaviorSubject<any> = new BehaviorSubject({});
+    periodoLectivo:BehaviorSubject<any> = new BehaviorSubject(0);
+    cursoDivision:BehaviorSubject<any> = new BehaviorSubject('');
+
     constructor(private fb:FormBuilder,private acadService:AcademicoService
         ,private messageService:MessageService,private router:Router
         ,private activateRoute:ActivatedRoute){
@@ -43,15 +44,20 @@ export class CargaExamenPromedio implements OnInit{
             .subscribe(data=>{
                 console.log('Retornarno examenes');
                 //this.examenes=data.examenes;
-                this.periodosEval.periodos=data.periodos;
-                this.periodosEval.keyFormControls=[];
-                this.periodosEval.labelArray=[];
-                this.alumno = data.alumno;
-                this.asignatura = data.asignatura;
+                let periodos=data.periodos;
+                let keyFormControls;
+                let labelArray;
+                this.alumno.next(data.alumno);
+                this.asignatura.next(data.asignatura);
+                this.periodoLectivo.next(data.periodoLectivo);
+                this.cursoDivision.next(data.cursoDivision);
+
                 
                 console.log("antes del error "+this.periodosEval[0]);
-                for(let per of this.periodosEval.periodos){ 
+                for(let per of periodos){ 
                     var i=0,examId=0,tipoExamen='';
+                    keyFormControls=[];
+                    labelArray=[];
                     for(let exam of per.examenes){
                         this.examenesForm.addControl(i+'_exam_'+exam.id+'_'
                                 +per.id+'_'+'exam'
@@ -60,31 +66,30 @@ export class CargaExamenPromedio implements OnInit{
                                 
                                         CustomValidators.validatePromedio(control)]  )      
                                 );
-                        this.periodosEval.keyFormControls.push(i+'_exam_'+exam.id+'_'
+                        keyFormControls.push(i+'_exam_'+exam.id+'_'
                                     +per.id+'_'+'exam'
                                 );                                
                         
-                        this.periodosEval.labelArray.push(per.descripcion+' '+exam.tipoExamen+':');
+                        labelArray.push(per.descripcion+' '+exam.descripcion+':');
 
 
                         examId=exam.id;
                         i++;
                     }
+                    
                     examId=per.examenes[per.examenes.length-1].id;
-                    tipoExamen=per.examenes[per.examenes.length-1].tipoExamen.descripcion
+                    tipoExamen=per.examenes[per.examenes.length-1].descripcion
                     this.examenesForm.addControl(i+'_exam_'+examId+'_'
                             +per.id+'_'+'inas'
-                        ,new FormControl(per.cantInasist,[Validators.required],
-                            [(control: AbstractControl): Observable<ValidationErrors | null> => 
-                            
-                                    CustomValidators.validatePromedio(control)]  )      
+                        ,new FormControl(per.cantInasist,[Validators.required])      
                             );
-                    this.periodosEval.keyFormControls.push(i+'_exam_'+examId+'_'
+                    keyFormControls.push(i+'_exam_'+examId+'_'
                                 +per.id+'_'+'inas'
                             );                                
                     
-                    this.periodosEval.labelArray.push(per.descripcion+' '+tipoExamen+':');
-                
+                    labelArray.push(per.descripcion+' Inasistencias:');
+                    per.keyFormControls = keyFormControls;
+                    per.labelArray=labelArray;
 
                     /*this.examenesForm.addControl((i-1)+'_exam_'+exam.id+'_'
                                 +per.id+'_'+'inas'
@@ -97,9 +102,11 @@ export class CargaExamenPromedio implements OnInit{
                     this.examKeys=Object.keys(this.examenesForm.controls);
                     */
                 }
+                this.periodosEval.next({periodos:periodos
+                    ,keyFormControls:keyFormControls,labelArray:labelArray});
 
             });
-            console.log('Periodos: '+this.periodosEval.periodos);
+            //console.log('Periodos: '+this.periodosEval.periodos);
     }
 
     onSubmit(valuesForm){
